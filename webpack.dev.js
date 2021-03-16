89bb311297eeeb3afeb2169bd15f39b6b8005b8f
Andrzej Łuszcz <endriu1297@gmail.com>
2021-03-16 21:10:00 +0100

    add webpack files

---
+++ webpack.dev.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const common = require('./webpack.common');
+const merge = require('webpack-merge');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'development',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: {
+        removeAttributeQuotes: true,
+        collapseWhitespace: true,
+        removeComments: true,
+      },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+});
