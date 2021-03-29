f02e0b786b9206be15b2a3b5ed42e34e8c6e28ff
Andrzej Łuszcz <endriu1297@gmail.com>
2021-03-29 22:03:07 +0200

    add resize for point on active

--- src/js/pointActiveSection.js
+++ src/js/pointActiveSection.js
@@ -58,11 +58,54 @@ const addListenWindow = (screenSize) => {
   });
 };
 
+const deleteListeners = () => {
+  const navItems = document.querySelectorAll('.nav__item');
+  const skillSection = document.querySelector('.section:nth-child(2)');
+  const projectSection = document.querySelector('.section:nth-child(3)');
+  const contactSection = document.querySelector('.section:nth-child(6)');
+  const skillRect = skillSection.getBoundingClientRect();
+  const projectRect = projectSection.getBoundingClientRect();
+  const contactRect = contactSection.getBoundingClientRect();
+  const skillOffsetTop = skillRect.y;
+  const projectOffsetTop = projectRect.y;
+  const contactOffsetTop = contactRect.y;
+  const heightSection = skillRect.height;
+  const screenWidth = window.innerWidth;
+  const screenSize = screenWidth < 2000;
+
+  [...navItems].map((item) => {
+    item.replaceWith(item.cloneNode(true));
+  });
+
+  window.removeEventListener('scroll', () => {
+    const size = screenSize ? 'small' : 'large';
+
+    if (window.pageYOffset < skillOffsetTop - heightSection / 2) {
+      hoverEvent(0, size);
+    } else if (
+      window.pageYOffset >= skillOffsetTop - heightSection / 2 &&
+      window.pageYOffset < projectOffsetTop - heightSection / 2
+    ) {
+      hoverEvent(1, size);
+    } else if (
+      window.pageYOffset >= projectOffsetTop - heightSection / 2 &&
+      window.pageYOffset < contactOffsetTop - heightSection / 2
+    ) {
+      hoverEvent(2, size);
+    } else if (window.pageYOffset >= contactOffsetTop - heightSection) {
+      hoverEvent(3, size);
+    }
+  });
+
+  pointActiveSection();
+};
+
 const pointActiveSection = () => {
   const navItems = document.querySelectorAll('.nav__item');
   const rect = navItems[0].getBoundingClientRect();
   const screenWidth = window.innerWidth;
   const screenSize = screenWidth < 2000;
+  let resizeTimer = undefined;
   widthItem = rect.width;
 
   if (screenSize) {
@@ -87,7 +130,15 @@ const pointActiveSection = () => {
     }
   });
 
-  addListenWindow(screenSize);
+  addListenWindow();
+
+  window.addEventListener('resize', () => {
+    clearTimeout(resizeTimer);
+
+    resizeTimer = setTimeout(() => {
+      deleteListeners();
+    }, 200);
+  });
 };
 
 export default pointActiveSection;
