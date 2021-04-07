81ab5ee03952e8919a225f0b5f5d4d7d8ba4f6ee
Andrzej Łuszcz <endriu1297@gmail.com>
2021-04-07 21:04:02 +0200

    add close navigation when click on nav__item in mobile devices

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,5 +1,17 @@
 const hamburger = document.querySelector('.hamburger');
+const navItems = document.querySelectorAll('.nav__item');
 const nav = document.querySelector('.nav');
+
+const initCloseNav = () => {
+  for (let i = 0; i < navItems.length; i++) {
+    navItems[i].addEventListener('click', () => {
+      if (window.innerWidth < 1024) {
+        hamburger.classList.remove('hamburger--active');
+        nav.style.transform = 'translateX(-100vw)';
+      }
+    });
+  }
+};
 
 const initHamburger = () => {
   hamburger.addEventListener('click', () => {
@@ -11,6 +23,8 @@ const initHamburger = () => {
       nav.style.transform = 'translateX(-100vw)';
     }
   });
+
+  initCloseNav();
 };
 
 export default initHamburger;
