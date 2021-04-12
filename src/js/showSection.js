94b842a6294a1dc1bffe211e98878adf915018f6
Andrzej Łuszcz <endriu1297@gmail.com>
2021-04-12 20:33:23 +0200

    add show animation for leftSide title

--- src/js/showSection.js
+++ src/js/showSection.js
@@ -2,26 +2,40 @@ const animationValues = '.5s linear forwards';
 const rightSideTitles = document.querySelectorAll(
   '.section__rightSide .section__title',
 );
+const leftSideTitles = document.querySelectorAll(
+  '.section__leftSide .section__title',
+);
 const rightDimensions = [...rightSideTitles].map((item) => {
   const rect = item.getBoundingClientRect();
   const itemTopOffset = rect.top;
 
   return itemTopOffset;
 });
+const leftDimensions = [...leftSideTitles].map((item) => {
+  const rect = item.getBoundingClientRect();
+  const itemTopOffset = rect.top;
 
-const addShowAnimation = (item, index) => {
+  return itemTopOffset;
+});
+const screenHeight = window.innerHeight;
+const animationNames = ['showTitleLeftSide', 'showTitleRightSide'];
+
+const addShowAnimation = (item, animationName, dimensions) => {
   const howFarScroll = window.pageYOffset;
-  const screenHeight = window.innerHeight;
 
-  if (howFarScroll >= rightDimensions[index] - screenHeight * 0.5) {
-    item.style.animation = `showTitleRightSide ${animationValues}`;
+  if (howFarScroll >= dimensions - screenHeight * 0.5) {
+    item.style.animation = `${animationName} ${animationValues}`;
   }
 };
 
 const showSection = () => {
   window.addEventListener('scroll', () => {
+    [...leftSideTitles].map((item, index) => {
+      addShowAnimation(item, animationNames[0], leftDimensions[index]);
+    });
+
     [...rightSideTitles].map((item, index) => {
-      addShowAnimation(item, index);
+      addShowAnimation(item, animationNames[1], rightDimensions[index]);
     });
   });
 };
