2ad9a1123dec25dfb33e7d38d8d7b73f3f2775df
Andrzej Łuszcz <endriu1297@gmail.com>
2021-04-12 20:42:53 +0200

    refactor code in showImages file

--- src/js/showImages.js
+++ src/js/showImages.js
@@ -1,9 +1,18 @@
+const profileImage = document.querySelector('.section__profileImage');
+const appImages = document.querySelectorAll('.section__appImage');
+const allImages = [profileImage, ...appImages];
+const dimensions = [...allImages].map((item) => {
+  const rect = item.getBoundingClientRect();
+  const itemTopOffset = rect.top;
+
+  return itemTopOffset;
+});
 const root = document.documentElement;
 const animationValue = '.5s linear forwards';
+const screenWidth = window.innerWidth;
+const screenHeight = window.innerHeight;
 
 const addProperlyAppAnimation = (image) => {
-  const screenWidth = window.innerWidth;
-
   if (screenWidth < 550) {
     image.style.animation = `showAppImageSmall ${animationValue}`;
   } else if (screenWidth < 1024) {
@@ -23,30 +32,23 @@ const addProperlyProfileAnimation = (image) => {
   }
 };
 
-const incScaleAnimation = (image, multipleBy = 4.75) => {
-  const rect = image.getBoundingClientRect();
-  const imageTopOffset = rect.top;
+const incScaleAnimation = (image, dimension, multipleBy = 4.75) => {
   const howFarScroll = window.pageYOffset;
 
-  if (howFarScroll - imageTopOffset === 0 && multipleBy === 1) {
+  if (howFarScroll >= dimension - screenHeight * 0.6 && multipleBy === 1) {
     addProperlyProfileAnimation(image);
-  } else if (howFarScroll >= imageTopOffset * multipleBy) {
+  } else if (howFarScroll >= dimension - screenHeight * 0.6) {
     addProperlyAppAnimation(image);
   }
 };
 
 const showImages = () => {
-  const profileImage = document.querySelector('.section__profileImage');
-  const appImages = document.querySelectorAll('.section__appImage');
-
   window.addEventListener('scroll', () => {
-    const screenWidth = window.innerWidth;
-
-    [profileImage, ...appImages].map((item, index) => {
+    allImages.map((item, index) => {
       if (index === 0) {
-        incScaleAnimation(item, 1);
+        incScaleAnimation(item, dimensions[index], 1);
       } else {
-        incScaleAnimation(item);
+        incScaleAnimation(item, dimensions[index]);
       }
     });
   });
