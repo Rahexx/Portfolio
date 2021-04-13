101630c2a37294a2ff65c9c0dcbb1d16168d87ff
Andrzej Łuszcz <endriu1297@gmail.com>
2021-04-13 18:40:10 +0200

    add show text in properly time

--- src/js/showSection.js
+++ src/js/showSection.js
@@ -5,13 +5,31 @@ const rightSideTitles = document.querySelectorAll(
 const leftSideTitles = document.querySelectorAll(
   '.section__leftSide .section__title',
 );
-const rightDimensions = [...rightSideTitles].map((item) => {
+const rightSideText = document.querySelectorAll(
+  '.section__rightSide .section__text',
+);
+const leftSideText = document.querySelectorAll(
+  '.section__leftSide .section__text',
+);
+const listTechnology = document.querySelectorAll(
+  '.section__rightSide .section__listTechnology',
+);
+const contactText = document.querySelectorAll(
+  '.section__rightSide .section__contact',
+);
+const itemsRightSide = [
+  ...rightSideTitles,
+  ...rightSideText,
+  ...listTechnology,
+  ...contactText,
+];
+const rightDimensions = itemsRightSide.map((item) => {
   const rect = item.getBoundingClientRect();
   const itemTopOffset = rect.top;
 
   return itemTopOffset;
 });
-const leftDimensions = [...leftSideTitles].map((item) => {
+const leftDimensions = [...leftSideTitles, ...leftSideText].map((item) => {
   const rect = item.getBoundingClientRect();
   const itemTopOffset = rect.top;
 
@@ -30,11 +48,11 @@ const addShowAnimation = (item, animationName, dimensions) => {
 
 const showSection = () => {
   window.addEventListener('scroll', () => {
-    [...leftSideTitles].map((item, index) => {
+    [...leftSideTitles, ...leftSideText].map((item, index) => {
       addShowAnimation(item, animationNames[0], leftDimensions[index]);
     });
 
-    [...rightSideTitles].map((item, index) => {
+    itemsRightSide.map((item, index) => {
       addShowAnimation(item, animationNames[1], rightDimensions[index]);
     });
   });
